138d8ee2fd8c834e1f8aab30012fbecbf524d0a0
Meteors27 <williamxia27@qq.com>
2022-12-06 00:20:24 +0800

    final version for homework 6

--- main.cpp
+++ main.cpp
@@ -1,18 +1,10 @@
-#include <iostream>
-#include "Laplace.h"
-#include "Filtering.h"
+#include "BilateralFilter.h"
 
 int main() {
-    Filtering filter;
-    Laplace laplace;
-    /* MeanFiltering */
-    auto img1 = filter.imgread("../images/Matterhorn.bmp");
-    auto img2 = filter.meanFiltering(img1, 9);
-    filter.imgwrite("../images/MeanFiltering.bmp",img2);
-    /* LaplaceEnhancement */
-    auto img3 = laplace.imgread("../images/greyscale.bmp");
-    auto img4 = laplace.LaplacianEnhance(img3);
-    laplace.imgwrite("../images/Laplace.bmp", img4);
-
+    BilateralFilter filter(12, 1);
+    /* Bilateral Filtering */
+    auto input = filter.imgread("../images/castle.bmp");
+    auto output = filter.bf(input);
+    filter.imgwrite("../images/BilateralFilter.bmp",output);
     return 0;
 }
